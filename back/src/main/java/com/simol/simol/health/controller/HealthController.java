60a3236cd1d06173586f095b4a0939aaf41aa76c
juno-choi <ililil9482@naver.com>
2024-03-25 23:50:57 +0900

    fix : /api prefix 추가

--- back/src/main/java/com/simol/simol/health/controller/HealthController.java
+++ back/src/main/java/com/simol/simol/health/controller/HealthController.java
@@ -2,9 +2,11 @@ package com.simol.simol.health.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<String> hello() {
