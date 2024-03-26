1f63d03c6191ec3f72a2e73011841e8fb6a93d0c
juno <juno.c@kakaovx.com>
2024-03-26 12:34:34 +0900

    chore : 깃허브 액션 테스트

--- back/src/main/java/com/simol/simol/health/controller/HealthController.java
+++ back/src/main/java/com/simol/simol/health/controller/HealthController.java
@@ -1,5 +1,6 @@
 package com.simol.simol.health.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<String> hello() {
+        log.info("health check");
         return ResponseEntity.ok("ok");
     }
 }
