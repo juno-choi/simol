46cb7f6f6987404d970801daa5b6e7964686baaa
juno.c <juno.c@kakaovx.com>
2025-02-10 11:06:39 +0900

    user project 추가

---
+++ oun-user/src/main/java/com/simol/oun_user/OunUserApplication.java
@@ -0,0 +1,13 @@
+package com.simol.oun_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OunUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OunUserApplication.class, args);
+	}
+
+}
