0f60e4dc26b92b72ac76f27da2052649fa84300b
juno-choi <ililil9482@naver.com>
2025-02-26 13:05:26 +0900

    feat: health set target,real  기본 정의 추가

--- oun-common/src/main/java/com/simol/ouncommon/health/dto/HealthSetCreateRequest.java
+++ oun-common/src/main/java/com/simol/ouncommon/health/dto/HealthSetCreateRequest.java
@@ -1,7 +1,5 @@
 package com.simol.ouncommon.health.dto;
 
-import java.util.Optional;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
---
+++ oun-common/src/main/java/com/simol/ouncommon/health/service/HealthSetRealService.java
@@ -0,0 +1,5 @@
+package com.simol.ouncommon.health.service;
+
+public interface HealthSetRealService {
+    
+}
