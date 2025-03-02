001f5b57907bddce18596a46c66fca2106c10071
juno-choi <ililil9482@naver.com>
2025-03-02 10:22:47 +0900

    chore: package 이동

--- oun-user/src/main/java/com/simol/ounuser/health/controller/HealthController.java
+++ oun-user/src/main/java/com/simol/ounuser/health/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.simol.ounuser.health;
+package com.simol.ounuser.health.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
