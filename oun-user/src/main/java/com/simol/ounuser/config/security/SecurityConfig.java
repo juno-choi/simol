58d88ccd55a57357e13afb17644c401a2d1df33b
juno-choi <ililil9482@naver.com>
2025-03-02 16:37:47 +0900

    fix: traefik 인증 filter 작동하도록 설정

--- oun-user/src/main/java/com/simol/ounuser/config/security/SecurityConfig.java
+++ oun-user/src/main/java/com/simol/ounuser/config/security/SecurityConfig.java
@@ -2,22 +2,41 @@ package com.simol.ounuser.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final Environment env;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        if (isLocal()) {
+            return http
+                .cors(cors -> cors.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(c -> c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable).disable())   // h2 설정
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(
+                        "/**"
+                    ).permitAll()
+                    .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2.disable())
+                .build();
+        }
+
         return http
             .cors(cors -> cors.disable())
             .csrf(AbstractHttpConfigurer::disable)
-            .headers(c -> c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable).disable())   // h2 설정
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                     "/**"
@@ -26,6 +45,16 @@ public class SecurityConfig {
             )
             .oauth2Login(oauth2 -> oauth2.disable())
             .build();
+    }
+
+    private boolean isLocal() {
+        String[] activeProfiles = env.getActiveProfiles();
+        for (String p : activeProfiles) {
+            if (p.equals("local")) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
