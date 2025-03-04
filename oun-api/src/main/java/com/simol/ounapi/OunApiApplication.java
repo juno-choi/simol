78d725d5ceb988972526fcbff7a3e8e181351b99
juno-choi <ililil9482@naver.com>
2025-03-05 08:10:23 +0900

    feat: querydsl bean을 user, api 프로젝트에서 적용하도록 처리

--- oun-api/src/main/java/com/simol/ounapi/OunApiApplication.java
+++ oun-api/src/main/java/com/simol/ounapi/OunApiApplication.java
@@ -3,10 +3,13 @@ package com.simol.ounapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
-@EntityScan(basePackages = {"com.simol.ouncommon"})
-@EnableJpaRepositories(basePackages = {"com.simol.ouncommon"})
+@ComponentScan(basePackages = {"com.simol.ouncommon.*", "com.simol.ounapi.*"})
+@EntityScan(basePackages = {"com.simol.ouncommon.*.entity"})
+@EnableJpaRepositories(basePackages = {"com.simol.ouncommon.*.repository"})
 public class OunApiApplication {
 
 	public static void main(String[] args) {
--- oun-user/src/main/java/com/simol/ounuser/OunUserApplication.java
+++ oun-user/src/main/java/com/simol/ounuser/OunUserApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.simol.ouncommon"})
-@EnableJpaRepositories(basePackages = {"com.simol.ouncommon"})
+@ComponentScan(basePackages = {"com.simol.ouncommon.*", "com.simol.ounuser.*"})
+@EntityScan(basePackages = {"com.simol.ouncommon.*.entity"})
+@EnableJpaRepositories(basePackages = {"com.simol.ouncommon.*.repository"})
 public class OunUserApplication {
 
 	public static void main(String[] args) {
