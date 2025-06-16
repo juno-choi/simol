359ddd7fb039b588fcf7745352a0f70e53701ea0
juno-choi <ililil9482@naver.com>
2025-06-16 16:15:46 +0900

    fix: kong 프로젝트 삭제

--- simol-common/src/main/java/com/simol/simolcommon/SimolCommonApplication.java
+++ simol-common/src/main/java/com/simol/simolcommon/SimolCommonApplication.java
@@ -3,11 +3,12 @@ package com.simol.simolcommon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.simol.simolcommon.common.*"})
-@EnableJpaRepositories(basePackages = {"com.simol.simolcommon.common.*"})
+@EntityScan(basePackages = {"com.simol.simolcommon.common.sium.*.*.entity"})
+@EnableJpaRepositories(basePackages = {"com.simol.simolcommon.common.sium.*.*.repository"})
 public class SimolCommonApplication {
 
 	public static void main(String[] args) {
