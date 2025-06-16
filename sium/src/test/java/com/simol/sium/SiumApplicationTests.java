6dc42d4f567ab2797f32eebd06f39f68cb84a3ab
juno-choi <ililil9482@naver.com>
2025-06-16 16:16:00 +0900

    feat: sium 프로젝트 추가

---
+++ sium/src/test/java/com/simol/sium/SiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simol.sium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
